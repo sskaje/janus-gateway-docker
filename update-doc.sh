ab29cb2a0d03e7c8ddc1d6e283e206a38492fbdd
sskaje <sskaje@gmail.com>
2025-01-15 13:24:14 +0800

    fix update-doc.sh

--- update-doc.sh
+++ update-doc.sh
@@ -5,7 +5,7 @@ docker create --name temp-container rst/janus-builder
 
 docker cp temp-container:/usr/local/etc/janus/. config
 docker cp temp-container:/usr/local/share/janus/html/. html
-docker cp temp-container:/tmp/docs/html/* html/docs/
+docker cp temp-container:/tmp/copy/docs/. html/docs
 
 docker rm temp-container
 # docker rmi rst/janus-builder
